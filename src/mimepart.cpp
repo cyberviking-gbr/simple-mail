59d4ed32113b39c5acea1e4e75a065d368b11bc6
Armin Leghissa <armin.leghissa@gmail.com>
2017-10-15 05:18:18 +0200

    fixed missing closing quote

--- src/mimepart.cpp
+++ src/mimepart.cpp
@@ -235,7 +235,9 @@ bool MimePart::write(QIODevice *device)
     // Content-Type
     headers.append("Content-Type: " + d->contentType);
     if (!d->contentName.isEmpty()) {
-        headers.append("; name=\"" + d->contentName);
+        headers.append("; name=\"");
+        headers.append(d->contentName);
+        headers.append("\"");
     }
     if (!d->contentCharset.isEmpty()) {
         headers.append("; charset=" + d->contentCharset);
